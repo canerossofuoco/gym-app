72b89bff5aba3e3220c91168453f21d17d2886e0
canerossofuoco <canerossofuoco@gmail.com>
2024-02-09 18:50:40 +0100

    aggiunta carta calorie, e fixato doc with uasard changes

---
+++ my-app/src/components/caloriesCard.tsx
@@ -0,0 +1,33 @@
+import { Progress } from "./progress";
+
+
+function CaloriesCard() {
+    
+    return ( 
+        <>
+            <div id="divCalories" className="w-full h-[33%] rounded-xl p-[4%] shadow-md border bg-secondary flex-col justify-between align-middle">
+                <p className="text-secondary-foreground mb-2 font-bold"  >Calories:</p>
+                <Progress value={65}/>
+                <div className=" pt-[6%]">
+                    <div className="p-[4%] flex ">
+                        <p className="text-secondary-foreground font-bold pr-[2%]">C </p>
+                        <div className="w-full pt-[3%] flex"><Progress value={70} className="w-2/3"/></div>
+                    </div>
+                    <div className="p-[4%] flex ">
+                        <p className="text-secondary-foreground font-bold pr-[2%]">P</p>
+                        <div className="w-full pt-[3%]"><Progress value={42} className="w-2/3"/></div>
+                    </div>
+                    <div className="p-[4%] flex ">
+                        <p className="text-secondary-foreground font-bold pr-[2%]">F</p>
+                        <div className="w-full pt-[3%]"><Progress value={10} className="w-2/3"/></div>
+                    </div>
+                </div>
+                {/* <p className="p-1">a</p>
+                <p className="p-1">b</p>
+                <p className="p-1">c</p> lol*/}
+            </div>
+        </>
+    )
+}
+
+export default CaloriesCard;
--- my-app/src/pages/Home.tsx
+++ my-app/src/pages/Home.tsx
@@ -2,11 +2,13 @@ import React, { useEffect,useState } from 'react';
 import '../index.css';
 import { Navigate, useNavigate } from 'react-router-dom';
 import Navbar from '../components/navbar';
+import CaloriesCard from '../components/caloriesCard';
 function Home() {
     return (
         <>
-        <div id="HomeDiv" className="h-[91%] w-full ">   
-        home
+        
+        <div id="HomeDiv" className="h-[91%] w-full p-[5%]">   
+            <CaloriesCard/>
         </div>
         <Navbar/>
         </>
