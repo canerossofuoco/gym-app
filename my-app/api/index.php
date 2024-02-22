4ef8dc2512e17948c47457456f04e0c192cef3d3
canerossolaptop <canerossofuoco@gmail.com>
2024-02-22 11:54:31 +0100

    finito tutto il backend, da testare.

--- my-app/api/index.php
+++ my-app/api/index.php
@@ -5,8 +5,12 @@
         '/loginUser' => 'loginUser',
         '/request/Calories' => 'requestCalories',
         '/add/workout' => 'addWorkout',
-        '/add/exercise/workout' => 'addExerciseToWorkout'
-    ); 
+        '/add/exercise' => 'addExercise',
+        '/add/exercise/workout' => 'addExerciseToWorkout',
+        '/request/profile' => 'requestProfile',
+        '/request/workout' => 'requestWorkout',
+        '/modify/exercise' => 'modifyExercise'
+    );
 
     $url =  $_SERVER['PHP_SELF'];
     $url = substr($url,10);
@@ -109,20 +113,115 @@
 
     }
 
-    function addExerciseToWorkout() {
-
-        $res = array("array risposta" => "addExerciseToWorkout");
-        
+    function addExercise() {
+        $res = array("array risposta" => "addExerciset");
         if(verify_cookie($_POST["cookie_id"],$_POST["cookie_email"])) {
             $nome = $_POST["nome"];
             $email = $_POST["cookie_email"];
-            $query = "select * from workouts where nome='$nome' and email_utente='$email';";
+            $query = "insert into esercizi values('$nome','$email','');";
+            $result_query = $conn->query($query);
+            if($result_query)
+                $res["inserimento"] = true;
+            else 
+                $res["inserimento"] = false;
+            echo json_encode($res);
+        }
+    }
+
+    function addExerciseToWorkout() {
+
+        $idw = -1;
+        $ide = -1;
+        
+        $res = array("array risposta" => "addExerciseToWorkout");
+        if(verify_cookie($_POST["cookie_id"],$_POST["cookie_email"])) {
+            $res["login"] = true;
+            $nome = $_POST["nome"];
+            $email = $_POST["cookie_email"];
+            $nome_esercizio = $_POST["nome_esercizio"];
+
+            $query = "select * from workouts where nome='$nome' and email_utente='$email';";  //seleziono id workout
             $res_query = $conn->query($query);
             if($result_query->num_rows==1) {
                 $row = $result->fetch_assoc();
-                $id = $row["id"];
+                $idw = $row["id"];
+            }
+
+            $query = "select * from esercizi where nome='$nome_esercizio' and email_utente='$email';"; //seleziono id esercizio
+            $res_query = $conn->query($query);
+            if($result_query->num_rows==1) {
+                $row = $result->fetch_assoc();
+                $ide = $row["id"];
             }
             
-        }
+            $query "insert into esercizi_workouts values($idw,$ide);"; //inserimento associazione workout-esercizio
+            $res_query = $conn->query($query);
+            if($result_query)
+                $res["inserimento"]  = true;
+            else 
+                $res["inserimento"] = false;
+        }else 
+            $res["login"] = false;
+        echo json_encode($res);
     }
+
+    function requestProfile() {
+        $res = array("array risposta" => "requestProfile"); 
+        if(verify_cookie($_POST["cookie_id"],$_POST["cookie_email"])) {
+            $res["login"] = true;
+            $email = $_POST["cookie_email"];
+            $query = "select * from utenti where email = '$email';";
+            $result_query = $conn->query($query);
+            if($result_query->num_rows==1) {
+                $row = $result->fetch_assoc();
+                $res["nome"] = $row["nome"];
+                $res["cognome"] = $row["cognome"];
+                $res["eta"] = $row["eta"];
+                $res["peso"] = $row["peso"];
+                $res["stringa_peso"] = $row["stringa_peso"];
+                $res["inserimento"] = true;
+            }else
+                $res["inserimento"] = false;
+        }else 
+            $res["login"] = false;
+        echo json_encode($res);
+    }
+    
+    function requestWorkout() {
+        $res = array("array risposta" => "requestWorkout");
+        if(verify_cookie($_POST["cookie_id"],$_POST["cookie_email"])) {
+            $res["login"] = true;
+            $email = $_POST["cookie_email"];
+            $query = "select * from workouts where email_utente='$email';";
+            $result_query = $conn->query($query);
+            if($result_query->num_rows>0) {
+                while($row=$result_query->fetch_assoc()) { //da verificare questa cosa
+                    $res["workouts"].=$row;
+                }
+            }else 
+                $res["workouts"] = "";
+        }else 
+            $res["login"] = false;
+        echo json_encode($res); 
+    }
+
+    function modifyExercise() {
+        
+        $res = array("array risposta" => "modifyExercise");
+        if(verify_cookie($_POST["cookie_id"],$_POST["cookie_email"])) {
+            $res["login"] = true;
+            $email = $_POST["cookie_email"];
+            $nome = $_POST["nome_esercizio"];
+            $peso = $_POST["stringa_peso"];
+            $query = "update esercizi set stringa_peso='$peso' where email_utente='$email' and nome='$nome';";
+            $result_query = $conn->query($query);
+            if($result_query) 
+                $res["inserimento"] = true;
+            else 
+                $res["inserimento"] = false;
+        }else 
+            $res["login"] = false;
+        echo json_encode($res);
+    }
+
 ?>
