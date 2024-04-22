cafd0249bc6b4acd33a216997f6116a41368c9ba
canerossolaptop <canerossofuoco@gmail.com>
2024-04-22 09:13:39 +0200

    modified the workout page

--- src/pages/Training.tsx
+++ src/pages/Training.tsx
@@ -20,60 +20,65 @@ function delay(ms: number) {
 
 function Training () {
 
-    const [resultString, setResultString] = useState('');
-    const [inputValue, setInputValue] = useState('');
+    const [color, setColor] = useState('secondary');
 
-    async function handleChange (e:any) {
-        setInputValue(e.target.value);
-        var query = e.target.value;
+    const [resultArray, setResultArray] = useState([]);
+
+    const handleChange = async (e:any) => {
+        const query = e.target.value;
         await delay(1000);
-        axios.get('https://api.api-ninjas.com/v1/exercises?muscle=' + query, {
-            headers: {
-                'X-Api-Key' : 'tsv4HCj8ZF9s+WmuQ109YA==IsL2iFlLPI9xwQuT',
-                'content-type': 'application/json',
-            }
-        })
-        .then((res) => {
-            res.data = JSON.stringify(res.data);
-            var array = JSON.parse(res.data) as {
-                name: "Incline Hammer Curls",
-                type: "strength",
-                muscle: "biceps",
-                equipment: "dumbbell",
-                difficulty: "beginner",
-                instructions: "Prova"
-            }[]
-            var stringa = "";
-            if(array.length!=0) {
-                for(var i = 0; i<array.length;i++) {
-                    stringa+='<div class="h-[10%] border rounded-xl bg-secondary mt-[4%] justify-around flex"> <p class="text-secondary-foreground font-bold pl-[1%] text-xl mt-[4%]">'+array[i].name+'</p> </div>';
+
+        try {
+            const response = await axios.get(`https://api.api-ninjas.com/v1/exercises?muscle=${query}`, {
+                headers: {
+                    'X-Api-Key': 'tsv4HCj8ZF9s+WmuQ109YA==IsL2iFlLPI9xwQuT',
+                    'Content-Type': 'application/json',
                 }
-                setResultString(stringa);
-            } else 
-                setResultString("");
-        })
-        .catch((err) => console.error(err));
-    }
+            });
+            const dataArray = response.data;
+
+            if (dataArray.length > 0) {
+                setResultArray(dataArray);
+            } else {
+                setResultArray([]);
+            }
+        } catch (error) {
+            console.error("Error fetching data:", error);
+        }
+    };
 
     return (
         <>
-        <div id="trainingDiv" className="h-[91%] w-full p-[5%]">
-        <Dialog>
-            <DialogTrigger className="bg-secondary border h-[10%] rounded-xl shadow-md flex justify-between w-full"> <AddWorkout/> </DialogTrigger>
-            <DialogContent className="rounded-xl h-[55%] overflow-x-hidden overflow-y-auto">
-                <DialogHeader>
-                <DialogTitle  className="h-[30%]"> 
-                    <Input type="text" placeholder="ExerciseName" className="mb-2 h-10" onChange={handleChange}/>  
-                </DialogTitle>
-                <DialogDescription  dangerouslySetInnerHTML={{ __html: resultString }}>
-                </DialogDescription>
-                </DialogHeader>
-            </DialogContent>
-        </Dialog>
-        </div>
-        <Navbar/>
+            <div id="trainingDiv" className="h-[91%] w-full p-[5%]">
+                <Dialog>
+                    <DialogTrigger className="bg-secondary border h-[10%] rounded-xl shadow-md flex justify-between w-full">
+                        <AddWorkout />
+                    </DialogTrigger>
+                    <DialogContent className="rounded-xl h-[55%] overflow-x-hidden overflow-y-auto">
+                        <DialogHeader>
+                            <DialogTitle className="h-[30%] flex">
+                                <Input type="text" placeholder="Exercise Name" className="mb-2 h-10 w-[80%]" onChange={handleChange} />
+                                <Button className="w-[20%] h-10">Done</Button>
+                            </DialogTitle>
+                            <DialogDescription className="h-[100%] w-[100%]">
+                                
+                                {resultArray.map((item, index) => (
+                                    <>
+                                    <div key={index} className={`h-[10%] border rounded-xl bg-${color} mt-[4%] justify-around flex`}>
+                                        {/* @ts-ignore */}
+                                        <p className="text-secondary-foreground font-bold pl-[1%] text-xl mt-[4%]" onClick={setColor("foreground")}>{item.name}</p>
+                                    </div>
+                                    
+                                    </>
+                                ))}
+                            </DialogDescription>
+                        </DialogHeader>
+                    </DialogContent>
+                </Dialog>
+            </div>
+            <Navbar />
         </>
     );
-}
+};
 
 export default Training;
